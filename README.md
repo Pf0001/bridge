cfe8c6e91bb43d640f2292a0673a69d5353adfd3
Pf0001 <2286421437@qq.com>
2023-03-09 15:16:15 +0800

    srb

---
+++ README.md
@@ -0,0 +1,161 @@
+# swarm_ros_bridge
+
+## Introduction
+
+A lightweight middle interface ROS package mainly based on [ZeroMQ](https://zeromq.org). It enables the specified ROS messages transmission among swarm robots through socket communication. The purpose of this package is to replace the traditional way of [running ROS across multiple machines in ROS1](https://wiki.ros.org/ROS/Tutorials/MultipleMachines), which has some drawbacks under swarm robots situation.
+
+An example of two ROS robots communicating with each other through swarm_ros_bridge is shown below:
+
+![framework](pictures/swarm_ros_bridge_framework.png)
+
+Compared with ROS1 multi-robot wireless communication, it has the following benefits:
+
+-  **Robust**: No need for base station ROS master launching first. Support each robot launching in a random sequence and connecting each other autonomously.
+
+-  **Flexible**:  You can choose the sending/receiving ROS topics rather than transferring all topic (names) as ROS1 does.
+
+-  **Easy to use**:  Specify all the IP and ROS topics in one configuration file.
+
+Compared with ROS2 DDS communication, it may have the following benefits:
+
+-  **Lightweight**: It is a small ROS bridge node subscribing and sending remote ROS topics, so connecting with other ROS nodes is easy.
+-  **Reliable**: It uses ZeroMQ socket communication based on TCP protocol while ROS2 is based on DDS, whose default protocol is UDP (unreliable). DDS is mainly designed for data exchange between native processes under wired communication rather than remote wireless communication.
+
+ROS wiki page: https://wiki.ros.org/swarm_ros_bridge
+
+source code: https://github.com/shupx/swarm_ros_bridge.git
+
+csdn blog (in chinese): https://blog.csdn.net/benchuspx/article/details/128576723
+
+
+## Structure
+
+```bash
+└── swarm_ros_bridge
+    ├── CMakeLists.txt
+    ├── config
+    │   └── ros_topics.yaml  # Config file to specify send/receive ROS topics
+    ├── include
+    │   ├── bridge_node.hpp  # Header file of bridge_node.cpp
+    │   ├── ros_sub_pub.hpp  # Header file for different ROS message type.
+    ├── launch
+    │   └── test.launch
+    ├── package.xml
+    └── src
+        └── bridge_node.cpp  # @brief Reliable TCP bridge for ros data transfer in unstable network.
+                             # It will send/receive the specified ROS topics in ../config/ros_topics.yaml
+                             # It uses zmq socket(PUB/SUB mode), which reconnects others autonomously and
+                             # supports 1-N pub-sub connection even with TCP protocol.
+```
+
+
+## Install
+
+**Supported platforms/releases**:
+
+| Platform                                                   | ROS Release                                                    |
+| ---------------------------------------------------------- | -------------------------------------------------------------- |
+| [Ubuntu 16.04 Xenial](https://releases.ubuntu.com/16.04.4/) | [ROS Kinetic](https://wiki.ros.org/kinetic/Installation/Ubuntu) |
+| [Ubuntu 18.04 Bionic](https://releases.ubuntu.com/18.04/) | [ROS Melodic](https://wiki.ros.org/melodic/Installation/Ubuntu) |
+| [Ubuntu 20.04 Focal](https://releases.ubuntu.com/20.04/) | [ROS Noetic](https://wiki.ros.org/noetic/Installation/Ubuntu) |
+
+**Install process**:
+
+```bash
+## clone this package
+mkdir -p swarm_ros_bridge_ws/src  # or your own ros workspace
+cd swarm_ros_bridge_ws/src
+git clone https://gitee.com/shu-peixuan/swarm_ros_bridge.git
+# or 'git clone https://github.com/shupx/swarm_ros_bridge.git'
+
+## install dependencies
+sudo apt install libzmqpp-dev
+# or 'rosdep install --from-path swarm_ros_bridge/'
+
+## build
+cd ../
+catkin_make
+source devel/setup.bash
+```
+
+
+## Usage
+
+1. Specify the IP and ROS topic information in `config/ros_topics.yaml`. 
+
+- For the sending topic, IP is self IP (* for example) and port should be different as it binds to the "tcp://*:port". 
+- For the receiving topic, IP and port should be the remote source IP and port as it connects to the "tcp://srcIP:srcPort".
+
+(The `max_freq` only guarantees the sending frequency is lower than that but not be that. If the send_topics frequency is larger than max_freq, the node will decrease it by 2x, 3x, ... until it satisfies the max_freq. check [github issue #2](https://github.com/shupx/swarm_ros_bridge/issues/2))
+
+2. Launch the bridge_node:
+
+```bash
+roslaunch swarm_ros_bridge test.launch
+```
+
+3. Publish messages into send_topics and check that remote recv_topics are receiving these messages. The console will also print INFO the first time recv_topics receive messages.
+
+
+## Advanced
+
+### * More ROS message types
+
+The default supported ROS message types are only `sensor_msgs/Imu` and `geometry_msgs/Twist`. If you need more types:
+
+1. Modify the macros about MSG_TYPEx and MSG_CLASSx in `include/ros_sub_pub.hpp`, then it will generate template functions for different ros message types.  
+
+```cpp
+// In ros_sub_pub.hpp
+// uncomment and modify the following lines:
+#include <xxx_msgs/yy.h>
+#define MSG_TYPE3 "xxx_msgs/yy"
+#define MSG_CLASS3 xxx_msgs::yy
+```
+
+We support up to 10 types modification. If that is still not enough, then you should modify the `topic_subscriber()`, `topic_publisher()` and `deserialize_publish()` in `include/ros_sub_pub.hpp` according to their styles.
+
+2. Add the dependent package in find_package() of `CMakeLists.txt`:
+
+```sh
+# in CMakeLists.txt
+find_package(catkin REQUIRED COMPONENTS
+  roscpp
+  std_msgs
+  geometry_msgs
+  sensor_msgs
+  xxx_msgs
+)
+```
+
+3. Recompile:
+
+```bash
+cd swarm_ros_bridge_ws/
+catkin_make
+```
+
+### * More send_topics
+
+We support up to 50 send_topics. Modify the following lines in `include/ros_sub_pub.hpp` if you need more.
+
+```cpp
+// in ros_sub_pub.hpp
+# define SUB_MAX 50 // max number of subscriber callbacks
+//...
+template <typename T>
+void (*sub_callbacks[])(const T &)=
+{
+  sub_cb<T,0>, sub_cb<T,1>, ... //add more
+};
+
+```
+
+Then recompile:
+
+```bash
+cd swarm_ros_bridge_ws/
+catkin_make
+```
+
+
---
+++ include/ros_sub_pub.hpp
@@ -0,0 +1,228 @@
+/**
+ * @file ros_sub_pub.hpp
+ * @author Peixuan Shu (shupeixuan@qq.com)
+ * @brief Header file for different ROS message type.
+ * 
+ * Core Idea: modify the macros about MSG_TYPEx and MSG_CLASSx,
+ *  it will generate template functions for different ros message types.
+ *  Remember to add the dependent package in find_package() of ../CMakeLists.txt
+ * 
+ * Note: the sub_cb() and deserialize_pub() are only declared here,
+ *  you should define them in you .cpp file according to your need.
+ * 
+ * @version 1.0
+ * @date 2023-01-01
+ * 
+ * @license BSD 3-Clause License
+ * @copyright (c) 2023, Peixuan Shu
+ * All rights reserved.
+ * 
+ */
+
+#ifndef __ROS_SUB_PUB__
+#define __ROS_SUB_PUB__
+#include <ros/ros.h>
+
+#include <sensor_msgs/Imu.h>
+#define MSG_TYPE1 "sensor_msgs/Imu"
+#define MSG_CLASS1 sensor_msgs::Imu
+
+#include <geometry_msgs/Twist.h>
+#define MSG_TYPE2 "geometry_msgs/Twist"
+#define MSG_CLASS2 geometry_msgs::Twist
+
+#include <sensor_msgs/PointCloud2.h>
+#define MSG_TYPE3 "sensor_msgs/PointCloud2"
+#define MSG_CLASS3 sensor_msgs::PointCloud2
+
+// #include <xxx/yy.h>
+// #define MSG_TYPE4 "xxx/yy"
+// #define MSG_CLASS4 xxx::yy
+
+// ......
+
+// #include <xxx/yy.h>
+// #define MSG_TYPE10 "xxx/yy"
+// #define MSG_CLASS10 xxx::yy
+
+# define SUB_MAX 50 // max number of subscriber callbacks
+
+template <typename T, int i>
+void sub_cb(const T &msg);
+
+template <typename T>
+void (*sub_callbacks[])(const T &);
+
+template <typename T>
+ros::Subscriber nh_sub(std::string topic_name, ros::NodeHandle nh, int i);
+
+ros::Subscriber topic_subscriber(std::string topic_name, std::string msg_type, ros::NodeHandle nh, int i);
+
+ros::Publisher topic_publisher(std::string topic_name, std::string msg_type, ros::NodeHandle nh);
+
+template<typename T>
+void deserialize_pub(uint8_t* buffer_ptr, size_t msg_size, int i);
+
+void deserialize_publish(uint8_t* buffer_ptr, size_t msg_size, std::string msg_type, int i);
+
+
+template <typename T>
+void (*sub_callbacks[])(const T &)=
+{
+  sub_cb<T,0>, sub_cb<T,1>, sub_cb<T,2>, sub_cb<T,3>, sub_cb<T,4>,
+  sub_cb<T,5>, sub_cb<T,6>, sub_cb<T,7>, sub_cb<T,8>, sub_cb<T,9>,
+  sub_cb<T,10>, sub_cb<T,11>, sub_cb<T,12>, sub_cb<T,13>, sub_cb<T,14>,
+  sub_cb<T,15>, sub_cb<T,16>, sub_cb<T,17>, sub_cb<T,18>, sub_cb<T,19>,
+  sub_cb<T,20>, sub_cb<T,21>, sub_cb<T,22>, sub_cb<T,23>, sub_cb<T,24>,
+  sub_cb<T,25>, sub_cb<T,26>, sub_cb<T,27>, sub_cb<T,28>, sub_cb<T,29>,
+  sub_cb<T,30>, sub_cb<T,31>, sub_cb<T,32>, sub_cb<T,33>, sub_cb<T,34>,
+  sub_cb<T,35>, sub_cb<T,36>, sub_cb<T,37>, sub_cb<T,38>, sub_cb<T,39>,
+  sub_cb<T,40>, sub_cb<T,41>, sub_cb<T,42>, sub_cb<T,43>, sub_cb<T,44>,
+  sub_cb<T,45>, sub_cb<T,46>, sub_cb<T,47>, sub_cb<T,48>, sub_cb<T,49>
+};
+
+template <typename T>
+ros::Subscriber nh_sub(std::string topic_name, ros::NodeHandle nh, int i)
+{
+  return nh.subscribe(topic_name, 10, sub_callbacks<T>[i], ros::TransportHints().tcpNoDelay());
+}
+
+ros::Subscriber topic_subscriber(std::string topic_name, std::string msg_type, ros::NodeHandle nh, int i)
+{
+  #ifdef MSG_TYPE1
+    if (msg_type == MSG_TYPE1)
+        return nh_sub<MSG_CLASS1>(topic_name, nh, i);
+  #endif
+  #ifdef MSG_TYPE2
+    if (msg_type == MSG_TYPE2)
+        return nh_sub<MSG_CLASS2>(topic_name, nh, i);
+  #endif
+  #ifdef MSG_TYPE3
+    if (msg_type == MSG_TYPE3)
+        return nh_sub<MSG_CLASS3>(topic_name, nh, i);
+  #endif
+  #ifdef MSG_TYPE4
+    if (msg_type == MSG_TYPE4)
+        return nh_sub<MSG_CLASS4>(topic_name, nh, i);
+  #endif  
+  #ifdef MSG_TYPE5
+    if (msg_type == MSG_TYPE5)
+        return nh_sub<MSG_CLASS5>(topic_name, nh, i);
+  #endif  
+  #ifdef MSG_TYPE6
+    if (msg_type == MSG_TYPE6)
+        return nh_sub<MSG_CLASS6>(topic_name, nh, i);
+  #endif  
+  #ifdef MSG_TYPE7
+    if (msg_type == MSG_TYPE7)
+        return nh_sub<MSG_CLASS7>(topic_name, nh, i);
+  #endif  
+  #ifdef MSG_TYPE8
+    if (msg_type == MSG_TYPE8)
+        return nh_sub<MSG_CLASS8>(topic_name, nh, i);
+  #endif  
+  #ifdef MSG_TYPE9
+    if (msg_type == MSG_TYPE9)
+        return nh_sub<MSG_CLASS9>(topic_name, nh, i);
+  #endif  
+  #ifdef MSG_TYPE10
+    if (msg_type == MSG_TYPE10)
+        return nh_sub<MSG_CLASS10>(topic_name, nh, i);
+  #endif  
+    ROS_FATAL("Invalid ROS msg_type \"%s\" in configuration!", msg_type.c_str());
+    exit(1);
+}
+
+ros::Publisher topic_publisher(std::string topic_name, std::string msg_type, ros::NodeHandle nh)
+{
+  #ifdef MSG_TYPE1
+    if (msg_type == MSG_TYPE1)
+        return nh.advertise<MSG_CLASS1>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE2
+    if (msg_type == MSG_TYPE2)
+        return nh.advertise<MSG_CLASS2>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE3
+    if (msg_type == MSG_TYPE3)
+        return nh.advertise<MSG_CLASS3>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE4
+    if (msg_type == MSG_TYPE4)
+        return nh.advertise<MSG_CLASS4>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE5
+    if (msg_type == MSG_TYPE5)
+        return nh.advertise<MSG_CLASS5>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE6
+    if (msg_type == MSG_TYPE6)
+        return nh.advertise<MSG_CLASS6>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE7
+    if (msg_type == MSG_TYPE7)
+        return nh.advertise<MSG_CLASS7>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE8
+    if (msg_type == MSG_TYPE8)
+        return nh.advertise<MSG_CLASS8>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE9
+    if (msg_type == MSG_TYPE9)
+        return nh.advertise<MSG_CLASS9>(topic_name, 10);
+  #endif
+  #ifdef MSG_TYPE10
+    if (msg_type == MSG_TYPE10)
+        return nh.advertise<MSG_CLASS10>(topic_name, 10);
+  #endif
+    ROS_FATAL("Invalid ROS msg_type \"%s\" in configuration!", msg_type.c_str());
+    exit(1);
+}
+
+void deserialize_publish(uint8_t* buffer_ptr, size_t msg_size, std::string msg_type, int i)
+{
+  #ifdef MSG_TYPE1
+    if (msg_type == MSG_TYPE1)
+        return deserialize_pub<MSG_CLASS1>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE2
+    if (msg_type == MSG_TYPE2)
+        return deserialize_pub<MSG_CLASS2>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE3
+    if (msg_type == MSG_TYPE3)
+        return deserialize_pub<MSG_CLASS3>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE4
+    if (msg_type == MSG_TYPE4)
+        return deserialize_pub<MSG_CLASS4>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE5
+    if (msg_type == MSG_TYPE5)
+        return deserialize_pub<MSG_CLASS5>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE6
+    if (msg_type == MSG_TYPE6)
+        return deserialize_pub<MSG_CLASS6>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE7
+    if (msg_type == MSG_TYPE7)
+        return deserialize_pub<MSG_CLASS7>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE8
+    if (msg_type == MSG_TYPE8)
+        return deserialize_pub<MSG_CLASS8>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE9
+    if (msg_type == MSG_TYPE9)
+        return deserialize_pub<MSG_CLASS9>(buffer_ptr, msg_size, i);
+  #endif
+  #ifdef MSG_TYPE10
+    if (msg_type == MSG_TYPE10)
+        return deserialize_pub<MSG_CLASS10>(buffer_ptr, msg_size, i);
+  #endif
+    ROS_FATAL("Invalid ROS msg_type \"%s\" in configuration!", msg_type.c_str());
+    exit(1);
+}
+
+#endif
